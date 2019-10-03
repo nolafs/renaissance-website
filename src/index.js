6b2663c69b7a69283efb0ce5521d6fd34455cf09
olaf <olaf@biffnewmedia.com>
2019-10-03 14:29:56 +0100

    - Add foundation 6

--- src/index.js
+++ src/index.js
@@ -21,25 +21,41 @@ Foundation.addToJquery($);
 
 /* Foundation Imports */
 import 'foundation-sites/dist/js/plugins/foundation.core';
-// import 'foundation-sites/js/foundation.util.keyboard';
-// import 'foundation-sites/js/foundation.accordion';
-//import 'foundation-sites/js/foundation.sticky';
-// import 'foundation-sites/js/foundation.util.mediaQuery';
-// import 'foundation-sites/js/foundation.offcanvas.js';
-// import 'foundation-sites/js/foundation.responsiveMenu';
-// import 'foundation-sites/js/foundation.util.triggers';
-// import 'foundation-sites/js/foundation.util.box';
-// import 'foundation-sites/js/foundation.util.nest';
-// import 'foundation-sites/js/foundation.abide';
-// import 'foundation-sites/js/foundation.util.motion';
-// import 'foundation-sites/js/foundation.toggler';
-// import 'foundation-sites/js/foundation.drilldown';
+import 'foundation-sites/dist/js/plugins/foundation.util.keyboard';
+import 'foundation-sites/dist/js/plugins/foundation.accordion';
+import 'foundation-sites/dist/js/plugins/foundation.sticky';
+import 'foundation-sites/dist/js/plugins/foundation.util.mediaQuery';
+import 'foundation-sites/dist/js/plugins/foundation.offcanvas.js';
+import 'foundation-sites/dist/js/plugins/foundation.responsiveMenu';
+import 'foundation-sites/dist/js/plugins/foundation.util.triggers';
+import 'foundation-sites/dist/js/plugins/foundation.util.box';
+import 'foundation-sites/dist/js/plugins/foundation.util.nest';
+import 'foundation-sites/dist/js/plugins/foundation.abide';
+import 'foundation-sites/dist/js/plugins/foundation.util.motion';
+import 'foundation-sites/dist/js/plugins/foundation.toggler';
+import 'foundation-sites/dist/js/plugins/foundation.drilldown';
+import 'foundation-sites/dist/js/plugins/foundation.equalizer';
+import 'foundation-sites/dist/js/plugins/foundation.responsiveAccordionTabs';
+import 'foundation-sites/dist/js/plugins/foundation.smoothScroll';
+import 'foundation-sites/dist/js/plugins/foundation.dropdown';
+import 'foundation-sites/dist/js/plugins/foundation.interchange';
+import 'foundation-sites/dist/js/plugins/foundation.magellan';
+import 'foundation-sites/dist/js/plugins/foundation.responsiveToggle';
+import 'foundation-sites/dist/js/plugins/foundation.reveal';
+import 'foundation-sites/dist/js/plugins/foundation.tabs';
+import 'foundation-sites/dist/js/plugins/foundation.tooltip';
+import 'foundation-sites/dist/js/plugins/foundation.util.imageLoader';
 
 
+$(document).foundation();
+// PAGE LOADING
+$( window ).on('load', function() {
+    $('.loading').fadeOut('fast');
+});
 
 // DOCUMENT READY
 $(document).ready(function () {
 
-    $(document).foundation();
+
 
 });
