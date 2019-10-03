c2ba0f96a0420788696bcaeba7f96f6ce31ea8c1
olaf <olaf@biffnewmedia.com>
2019-10-03 18:11:54 +0100

    - Add foundation 6

--- src/js/cms.js
+++ src/js/cms.js
@@ -2,6 +2,6 @@ import React from "react";
 import CMS from "netlify-cms-app";
 
 // Import main site styles as a string to inject into the CMS preview pane
-import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../scss/app.scss";
+import "../scss/app.scss";
 
 CMS.init();
