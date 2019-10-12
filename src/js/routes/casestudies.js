9524ad8d258c2c3b5879ded36a5c204d560a7a7b
olaf <olaf@biffnewmedia.com>
2019-10-12 18:31:34 +0100

    - case studies - list hash filtering added

--- src/js/routes/casestudies.js
+++ src/js/routes/casestudies.js
@@ -4,10 +4,28 @@ import $ from 'jquery'
 export default {
     init() {
         // JavaScript to be fired on all pages
-        console.log('case studies')
+        console.log('case studies', window.location.hash.substr(1))
+        let hash = window.location.hash.substr(1).toLowerCase()
+        if(!hash){
+            window.location.hash = "all";
+            hash = "all"
+        } else {
+            $('#filters .isActive').removeClass('isActive');
+            $(`#filter-${hash}`).addClass('isActive');
+        }
+
+        $( window ).on( 'hashchange', function( e ) {
+            console.log( 'hash changed' );
+            let temp = window.location.hash.substr(1).toLowerCase();
+            if(hash !== temp) {
+                $('#filters .isActive').removeClass('isActive');
+                $(`#filter-${temp}`).addClass('isActive');
+            }
+            hash = window.location.hash;
+        } );
     },
     finalize() {
-        // JavaScript to be fired on all pages, after page specific JS is fired
+        // JavaScript to be fired on all pages, after page specific JS is fire
 
         setTimeout(() => {
             const sizer = $('.my-sizer-element');
@@ -18,11 +36,18 @@ export default {
                 useTransforms: true,
             });
 
+            const hash = window.location.hash.substr(1).toLowerCase()
+
+            if(!hash || hash !== 'all'){
+                shuffle.filter(hash)
+            }
+
             $('#filters a').on('click', function ($event) {
                 $event.preventDefault();
                 const b = $(this);
                 const cat = (b.data('cat')).toLowerCase();
                 console.log(cat);
+                window.location.hash = cat;
                 if(cat !== 'all') {
                     shuffle.filter(cat)
                 } else {
