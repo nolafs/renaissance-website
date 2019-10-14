d36c23aebef30163e4508be508e72f4985958573
olaf <olaf@biffnewmedia.com>
2019-10-14 12:08:19 +0100

    - Add consent options to the site

---
+++ src/js/cms-previews-templates/case-studies.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+/*
+const headerEntry = ({heading, client, image}) =>
+    <div className="grid-container full" id="root">
+
+        <div className="grid-container full block header single">
+            <div className="grid-container">
+                <div className="grid-x grid-padding-x grid-margin-y align-middle">
+                    <div className="cell small-12 medium-8">
+                        <img src="{ image }" alt="Synced: Off Planet" />
+                    </div>
+                    <div className="cell small-12 medium-4 align-self-center f-c-black single-header">
+                        <h2><small>Go-to-Market</small>{heading}</h2>
+                        <p>{client}</p>
+
+                    </div>
+                </div>
+                <div className="lightning">
+                    <img src="/images/lightning-section.png" />
+                </div>
+            </div>
+        </div>;
+*/
+
+export default class CaseStudies extends React.Component {
+
+    render() {
+        const {entry, getAsset, widgetFor} = this.props;
+        return <div className="grid-container single-article">
+        <div className="grid-x grid-padding-x">
+            <div className="cell small-12 medium-8">
+             <article>
+                { widgetFor("body") }
+            </article>
+            </div>
+            <div className="cell small-12 medium-4">
+                aside
+            </div>
+        </div>;
+
+    }
+}
--- src/js/cms.js
+++ src/js/cms.js
@@ -3,5 +3,10 @@ import CMS from "netlify-cms-app";
 
 // Import main site styles as a string to inject into the CMS preview pane
 import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../scss/app.scss";
+
+import CaseStudies from "./cms-previews-templates/case-studies";
+
 CMS.registerPreviewStyle(styles, { raw: true });
+CMS.registerPreviewTemplate("case-stuides",CaseStudies);
+
 CMS.init();
