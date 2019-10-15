838dbf3cc7549b8c0e68c949eafbbe2a448c3b15
olaf <olaf@biffnewmedia.com>
2019-10-15 13:48:32 +0100

    Form validation added

--- src/js/routes/contact.js
+++ src/js/routes/contact.js
@@ -1,19 +1,43 @@
-import $ from 'jquery'
+import $ from 'jquery';
+import 'jquery-validation';
 export default {
     init() {
-        $('#thanks').hide()
-        console.log('CONTACT')
-        $("#contact").submit(function(e) {
-            e.preventDefault();
-            const $form = $(this);
-            $.post($form.attr("action"), $form.serialize()).then(function() {
-                $('#form').fadeOut(500, () => $('#thanks').fadeIn(1000))
-            });
+        $('#thanks').hide();
+        $('#sending').hide();
+
+        $("form[name='contact']").validate({
+            rules : {
+                name: 'required',
+                telephone: 'required',
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: 'Please enter your full name',
+                email: 'Please enter a valid email address',
+                telephone: 'Please enter your telephone number'
+            },
+            submitHandler: (form) => {
+                form.submit(() => {
+                    e.preventDefault();
+                    $('#form').fadeOut(100, () => $('#sending').fadeIn(100))
+                    const $form = $(this);
+                    $.post($form.attr("action"), $form.serialize()).then(function () {
+                        $('#sending').fadeOut(500, () => $('#thanks').fadeIn(500))
+                    });
+                })
+            }
         });
 
+
+
         $('#continue').on('click' , () => {
-            $('#thanks').fadeOut(500, () => $('#form').fadeIn(1000))
+            $('#thanks').fadeOut(500, () => $('#form').fadeIn(500))
         })
+
+
     },
     finalize() {
 
