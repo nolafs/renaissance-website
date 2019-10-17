650b3455682824352c77ccefa789c850cb397983
olaf <olaf@biffnewmedia.com>
2019-10-17 17:31:01 +0100

    Robot txt added

--- src/js/routes/common.js
+++ src/js/routes/common.js
@@ -1,14 +1,6 @@
-//import CC from 'CookieConsent'
-import $ from "jquery";
-
 export default {
   init() {
-    // JavaScript to be fired on all pages
-
   },
   finalize() {
-    //const cc = new CC();
-
-    $('#oS').foundation('down', ('#i-0'));
   },
 };
