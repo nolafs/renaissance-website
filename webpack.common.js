75faffc3c923e62aec8001c08b2a75129f4b2e8d
nolafs <Sontag2010>
2023-04-20 15:58:28 +0200

    fix: npm

--- webpack.common.js
+++ webpack.common.js
@@ -17,12 +17,17 @@ module.exports = {
   },
 
   resolve: {
-    extensions: ['*', '.js']
+    alias: {
+      'TweenLite': path.resolve('node_modules', 'gsap/gsap'),
+      'TweenMax': path.resolve('node_modules', 'gsap/gsap'),
+      'TimelineLite': path.resolve('node_modules', 'gsap/gsap'),
+      'TimelineMax': path.resolve('node_modules', 'gsap/gsap'),
+      'ScrollMagic': path.resolve('node_modules', 'scrollmagic/scrollmagic/minified/ScrollMagic.min.js'),
+      'animation.gsap': path.resolve('node_modules', 'scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js'),
+      'debug.addIndicators': path.resolve('node_modules', 'scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js')
+    }
   },
 
-  externals: {
-    Fluidbox: "fluidbox"
-  },
 
   module: {
     rules: [
